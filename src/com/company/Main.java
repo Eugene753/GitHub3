f80dcbf006f46896553372ce538371cab1bacc0b
Eugene753 <Imarkadanov@gmail.com>
2021-06-12 14:10:40 -0400

    my commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	Car car=new Car();
+
+	car.color="blue";
+	car.number="999";
+	car.door="6";
+	car.model="Audi";
+	dsfgdfgdfggdf
+
     }
 }
